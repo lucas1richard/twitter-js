4c05013a401d505b75ddf375d22a6556b220f0e6
lucas1richard <=>
2017-01-25 17:31:16 -0500

    added sockets.io

--- app.js
+++ app.js
@@ -4,30 +4,29 @@ const express = require('express');
 const app = express();
 const chalk = require('chalk');
 const nunjucks = require('nunjucks');
-const bodyParser = require('body-parser');
 const routes = require('./routes');
+const socketio = require('socket.io');
 
 const port = process.env.PORT || 3000;
+let server = app.listen(port, () => {
+  console.log(chalk.yellow(chalk.bold(`Server is listening on port ${port}`)));
+});
 
-app.use('/', routes);
+let io = socketio.listen(server);
+app.use('/', routes(io));
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', { noCache: true });
 
 
-
 // Handle Server Error
 app.use((err, req, res, next) => {
-  console.log(err.stack);
+  console.log(chalk.red(chalk.bold(err.stack)));
   console.log(chalk.yellow(req.method), chalk.red(chalk.bold(req.url)), chalk.red(500));
-  next();
 });
 
 // Handle Not Found
 app.use((req, res, next) => {
   console.log(chalk.yellow(req.method), chalk.red(chalk.bold(req.url)), chalk.red(404));
   next();
-});
-app.listen(port, () => {
-  console.log(chalk.yellow(chalk.bold(`Server is listening on port ${port}`)));
 });
--- routes/index.js
+++ routes/index.js
@@ -7,43 +7,48 @@ const tweetBank = require('../tweetBank');
 const path = require('path');
 
 let urlencodedParser = bodyParser.urlencoded({ extended: false });
+module.exports = function(io) {
+  console.log(io);
+  router.use('/stylesheets', function(req, res, next) {
+    console.log('/stylesheets');
+    next();
+  });
 
-router.use('/stylesheets', function(req, res, next) {
-  console.log('/stylesheets');
-  next();
-});
+  router.use('/stylesheets', express.static(path.join(__dirname, '../public/stylesheets')));
 
-router.use('/stylesheets', express.static(path.join(__dirname, '../public/stylesheets')));
+  router.get('/', function (req, res) {
+    let tweets = tweetBank.list();
+    res.render( 'index', { tweets } );
+  });
 
-router.get('/', function (req, res) {
-  let tweets = tweetBank.list();
-  res.render( 'index', { tweets } );
-});
+  router.get( '/users/:name', function (req, res) {
+    var name = req.params.name;
+    var tweets = tweetBank.find( { name: name } );
+    console.log(name, tweets);
+    res.render( 'index', { name: req.params.name, tweets, showForm: true } );
+  });
 
-router.get( '/users/:name', function (req, res) {
-  var name = req.params.name;
-  var tweets = tweetBank.find( { name: name } );
-  console.log(name, tweets);
-  res.render( 'index', { name: req.params.name, tweets, showForm: true } );
-});
+  router.get( '/tweets/:id', function (req, res) {
+    var id = req.params.id * 1;
+    var tweets = tweetBank.find( { id: id } );
+    res.render( 'index', { tweets } );
+  });
 
-router.get( '/tweets/:id', function (req, res) {
-  var id = req.params.id * 1;
-  var tweets = tweetBank.find( { id: id } );
-  res.render( 'index', { tweets } );
-});
+  router.post('/tweets', urlencodedParser, function(req, res) {
+    if (req.body) {
+      console.log(req.body);
+    } else {
+      console.log(req);
+    }
+    var name = req.body.name;
+    var text = req.body.text;
+    tweetBank.add(name, text);
+    // res.redirect('/');
+    io.sockets.emit('newTweet', { name, text });
 
-router.post('/tweets', urlencodedParser, function(req, res) {
-  if (req.body) {
-    console.log(req.body);
-  } else {
-    console.log(req);
-  }
-  var name = req.body.name;
-  var text = req.body.text;
-  tweetBank.add(name, text);
-  res.redirect('/');
-});
+  });
 
+  return router;
+};
 
-module.exports = router;
+// module.exports = router;
