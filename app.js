7eab0e4af7dc8bb3d7f2951014cdfdc34a4573e9
lucas1richard <=>
2017-01-24 21:32:27 -0500

    added templating engine

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 const chalk = require('chalk');
+const nunjucks = require('nunjucks');
+
+// console.log(nunjucks);
 
 const port = process.env.PORT || 3000;
+
+let locals = {
+  title: 'An Example',
+  people: [
+    { name: 'Gandalf' },
+    { name: 'Frodo' },
+    { name: 'Hermione' },
+  ]
+};
+
+// nunjucks.render('index.html', locals, (err, output) => {
+//   if (err) {
+//     console.log(chalk.red(err));
+//   } else {
+//     console.log(output);
+//   }
+// });
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true });
 
 app.use((req, res, next) => {
   res.status(200);
   console.log(chalk.magenta(req.method), chalk.bold(req.url), res.statusCode);
   next();
 });
-
 app.get('/', (req, res) => {
-  res.send('I\'m the homepage');
+  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
+
 });
 
 app.listen(port, () => {
